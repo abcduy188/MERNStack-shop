c02f15ec80e426f9cad7af81069f40638058225b
abcduy <tr.khanhsduy@gmail.com>
2022-03-07 11:35:28 +0700

    show detail product

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Routes , Route} from 'react-router-dom';
 import Product from './product/Product';
+import DetailProduct from './detailProduct/DetailProduct';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import Cart from './cart/Cart';
@@ -9,6 +10,7 @@ function Pages() {
   return (
     <Routes>
       <Route path='/' element={<Product />} />
+      <Route path='/detail/:id' element={<DetailProduct />} />
       <Route path='/login' element={<Login/>} />
       <Route path='/register'  element={<Register/>} />
       <Route path='/cart' element={<Cart/>} />
