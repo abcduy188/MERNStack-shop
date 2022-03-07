848147b64bed99e9e37b5835dc311d248f8aa360
abcduy <tr.khanhsduy@gmail.com>
2022-03-07 11:46:26 +0700

    effect loading page

--- client/src/components/mainpages/product/Product.js
+++ client/src/components/mainpages/product/Product.js
@@ -1,19 +1,24 @@
 import React,{useContext} from 'react';
 import {GlobalState} from '../../../GlobalState';
 import ProductItem from '../utils/ProductItem/ProductItem';
+import Loading from '../utils/loading/Loading';
+
 function Product() {
   const state = useContext(GlobalState);
   const [products] = state.productAPI.products
   console.log(state);
   return (
-    <div className='products'>
+    <>
+     <div className='products'>
         {
           products.map(product=>{
             return <ProductItem key={product._id} product={product}/>
           })
         }
     </div>
-    
+    {products.length === 0 && <Loading />}
+    </>
+   
   )
 }
 
